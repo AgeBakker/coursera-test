b1d257d31398daa9ae06acb449a6874acdce4e6f
AgeBakker <agebakk@online.no>
2024-01-20 14:46:05 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -116,10 +116,10 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //home
       
-      console.log(homeHtml);
-      console.log("CHOSEN: " + chosenCategoryShortName);
+      
+      console.log(chosenCategoryShortName);
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
-      console.log(homeHTML);
+      
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
